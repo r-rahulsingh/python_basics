8c261ee7548569804fc5d71f029c650ae88836ca
Rahul Singh <rs1.rahulsingh1@gmail.com>
2024-06-11 00:34:51 +0530

    2nd commit

---
+++ lists.py
@@ -0,0 +1,13 @@
+names = ["Harry","Ron","Hermione","Ginny"]
+
+print(names)
+
+print(names[0])
+
+names.append("Draco")
+
+print(names)
+
+names.sort()
+
+print(names)
---
+++ loops.py
@@ -0,0 +1,14 @@
+for i in [0,1,2,3,4,5]:
+    print(i)
+
+print(" ")
+
+for i in range(6):
+    print(i)
+
+names = ["Harry","Ron","Hermione","Ginny"]
+
+print(" ")
+
+for name in names:
+    print(name)
